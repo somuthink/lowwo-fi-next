8f4ebdc655a3a620a6e12d4ff892faa6f824a4e1
sxund <@qould12@gmail.com>
2023-08-26 14:09:31 +0500

    google analytics🍄

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import { getCards, getPlaylists } from "@/api/notion";
 import { Player } from "@/components/Player";
+import Script from "next/script";
 
 export default async function Home() {
   const [cards, playlists] = await Promise.all([getCards(), getPlaylists()]);
 
   return (
     <main className="overflow-hidden">
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-X8LW8FSVPY" strategy="afterInteractive"></Script>
+      <Script strategy="afterInteractive">
+        {`  window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-X8LW8FSVPY');`}
+      </Script>
       <Player cards={cards} playlists={playlists} />
     </main>
   );
