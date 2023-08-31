dd732df2b17d2d4e67914d3d9e60f266c7193413
sxund <@qould12@gmail.com>
2023-08-31 14:31:19 +0500

    CEO🔎

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import Head from 'next/head';
 import { Montserrat } from 'next/font/google'
+import Script from "next/script";
 
 const montserrat = Montserrat({ 
   subsets: ['latin'],
@@ -10,6 +12,7 @@ const montserrat = Montserrat({
 export const metadata: Metadata = {
   title: 'Lowwo-Fi',
   description: 'Craft Your Unique Ambience: Explore Lofi Playlists and a Variety of Customizable Sounds 🎶🍄',
+  
 }
 
 export default function RootLayout({
@@ -19,6 +22,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="yandex-verification" content="321d444d40c45309" />
+        <meta name="google-site-verification" content="cCUEuFtwrLIcYWMjmrJOY1vK3nMYoNTj-_glScMreeQ" />
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-X8LW8FSVPY" strategy="afterInteractive"></Script>
+        <Script strategy="afterInteractive">
+          {`  window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-X8LW8FSVPY');`}
+        </Script>
+      </Head>
       <body className={`${montserrat.variable}`}>{children}</body>
     </html>
   )
